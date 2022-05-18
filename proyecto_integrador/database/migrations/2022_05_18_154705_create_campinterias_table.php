ebe723596aa74a43f8dea2683d973ef38275b2a0
jorgeluis1122 <jorgeluissebastianh@gmail.com>
2022-05-18 12:04:54 -0500

    emmma

--- proyecto_integrador/database/migrations/2022_05_18_154705_create_campinterias_table.php
+++ proyecto_integrador/database/migrations/2022_05_18_154705_create_campinterias_table.php
@@ -3,27 +3,22 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+//hola
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('campinterias', function (Blueprint $table) {
             $table->id();
+            $table->string('Nombre',20);
+            $table->string('Telefono',10);
+            $table->string('Calle',20);
+            $table->string('NumeroExterior',6);
+            $table->string('Colonia',30);
+            $table->string('Municipio',15);
             $table->timestamps();
         });
     }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('campinterias');
