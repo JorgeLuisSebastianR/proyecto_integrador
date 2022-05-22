4f643abdf0a08a3313e992c1c723ff115144381b
Marco20TE05412 <rodriguezmendezmarcoantonioh@gmail.com>
2022-05-22 16:24:44 -0500

    juju

--- proyecto_integrador/resources/views/articulos/index.blade.php
+++ proyecto_integrador/resources/views/articulos/index.blade.php
@@ -5,9 +5,10 @@
 <section class="hero is-link">
     <div class="hero-body">
         <p class="title">
+            
             Agregar Articulo
         </p>
-        <a class="button is-info" href="/articulo/create">Agregar</a>
+        <a class="button is-info" href="/articulo/create">Agregar </a>
     </div>
 </section>
 
