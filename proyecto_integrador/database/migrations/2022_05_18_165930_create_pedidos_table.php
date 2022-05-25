a41f89c14866a5457f6f19f58ca3e95ac06bf64c
emmanueltejedatejeda <emmanuel.2d.28@gmail.com>
2022-05-25 09:31:15 -0500

    25/05/2022

--- proyecto_integrador/database/migrations/2022_05_18_165930_create_pedidos_table.php
+++ proyecto_integrador/database/migrations/2022_05_18_165930_create_pedidos_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('pedidos', function (Blueprint $table) {
             $table->id();
             $table->date('Fecha');
-            $table->time('Hota')
+            $table->time('Hota');
             $table->timestamps();
         });
     }
---
+++ proyecto_integrador/resources/views/maderas/create.blade.php
@@ -0,0 +1,15 @@
+@extends('layout.layout')
+@section('content')
+<section class="hero is-link">
+  <div class="hero-body">
+    <p class="title">
+        Formulario para llenar datos de maderas
+    </p>
+  </div>
+</section>
+
+<section class="">
+  <form class="" action="index.html" method="post">
+
+  </form>
+</section>
