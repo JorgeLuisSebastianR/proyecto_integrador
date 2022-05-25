5314345be6165b9ba6b7b41ea49beab900c8f0ba
emmanueltejedatejeda <emmanuel.2d.28@gmail.com>
2022-05-25 14:14:13 -0500

    xd

--- proyecto_integrador/routes/web.php
+++ proyecto_integrador/routes/web.php
@@ -5,20 +5,18 @@ use App\Http\Controllers\MaderaController;
 use App\Http\Controllers\CampinteriaController;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\HerramientaController;
-<<<<<<< HEAD
 
-=======
->>>>>>> c1e6b2f8ac38af5473eaf942a68e79649bbea83c
 
 Route :: resources([
 
 ]);
 
 Route::resources([
-    'campinterias' => CampinteriaController::class,
-    'customers' => CustomerController::class,
-    'articulos'=> ArticuloController :: class,
-    'maderas'=> MaderaController :: class,
+    'campinterias'  => CampinteriaController::class,
+    'customers'     => CustomerController::class,
+    'articulos'     => ArticuloController::class,
+    'maderas'       => MaderaController::class,
+    'pedidos'       => PedidoController::class
 ]);
 
 
@@ -33,10 +31,7 @@ Route::get('/', function () {
 });
 
 //Route::resource('campinterias', 'CampinteriaController');
-<<<<<<< HEAD
-=======
 
->>>>>>> 61ab57939fdaf7293e3e521984102e2651152f69
 Route::get('campinterias-pdf', 'CampinteriaController@exportToPDF')->name('campinterias.pdf');
 Route::get('customers-pdf', 'CustomerController@exportToPDF')->name('customers.pdf');
 
