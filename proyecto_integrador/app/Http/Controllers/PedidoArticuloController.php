522a574af746457c9287024106ac4aefd8c46452
jorgeluis1122 <jorgeluissebastianh@gmail.com>
2022-05-28 20:21:02 -0500

    .

--- proyecto_integrador/app/Http/Controllers/PedidoArticuloController.php
+++ proyecto_integrador/app/Http/Controllers/PedidoArticuloController.php
@@ -12,7 +12,7 @@ class PedidoArticuloController extends Controller
 
     public function index()
     {
-        //
+          return view('pedidoarticulos.index');
     }
 
     public function create()
--- proyecto_integrador/resources/views/pedidos/index.blade.php
+++ proyecto_integrador/resources/views/pedidos/index.blade.php
@@ -46,7 +46,7 @@
                      <!--comulma para mostrar la hora de registro-->
                         <td>{{ $pedido->Hora }}</td>
                         <td align="center">
-                        <a class="button is-success is-small" href="/pedido_articulos/index">Agregar</a>
+                        <a class="button is-success is-small" href="/pedidoarticulos/index">Agregar</a>
                         <td align="center">
                            <form action="{{route('pedidos.destroy',$pedido->id)}}" method="POST">
                                 <a class="button is-success is-small" href="{{route('pedidos.show',$pedido->id)}}">Ver</a>
