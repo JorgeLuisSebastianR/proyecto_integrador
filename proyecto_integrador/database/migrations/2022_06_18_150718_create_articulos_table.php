ca6fd5ccc56ed8698200e43b7f12b19f9b451e3f
oscarLS-11 <oluna1103@gmail.com>
2022-05-30 23:09:11 -0500

    .

--- proyecto_integrador/database/migrations/2022_06_18_150718_create_articulos_table.php
+++ proyecto_integrador/database/migrations/2022_06_18_150718_create_articulos_table.php
@@ -20,13 +20,8 @@ return new class extends Migration
             $table->foreign('idMadera')->references('id')->on('maderas');
             //campos.
             $table->text('Nombre',20);
-<<<<<<< HEAD
             $table->string('Tamaño', 20);
             $table->text('Color',20);
-=======
-            $table->string('Tamaño',15);
-            $table->string('Color',20);
->>>>>>> 44998cb4ec74dce699a9e3ae018c75386acf34d0
             $table->decimal('Precio', 8, 2, true)->default(0.00);
             $table->text('Descripcion',50)->nullable();
             $table->string('Tipo', 20) ;
